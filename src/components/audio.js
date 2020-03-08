a92185a6fd33ecfa123ad734711ecdfb78cf03de
Kenny Baskett <kbaskett248@gmail.com>
2020-03-08 11:46:42 -0400

    Add time to the play button

--- src/components/audio.js
+++ src/components/audio.js
@@ -3,16 +3,30 @@ import React from "react"
 
 class AudioPlayer extends React.Component {
     state = {
-        play: false
+        play: false,
+        currentTime: 0,
+        duration: 0
     }
     audio = new Audio(this.props.src);
 
     componentDidMount() {
+        this.audio.addEventListener('canplaythrough', this.updateDuration)
         this.audio.addEventListener('ended', () => this.setState({ play: false }));
+        this.audio.addEventListener('timeupdate', this.updateTime);
     }
 
     componentWillUnmount() {
+        this.audio.removeEventListener('canplaythrough', this.updateDuration)
         this.audio.removeEventListener('ended', () => this.setState({ play: false }));
+        this.audio.removeEventListener('timeupdate', this.updateTime);
+    }
+
+    updateTime = () => {
+        this.setState({ currentTime: this.audio.currentTime });
+    }
+
+    updateDuration = () => {
+        this.setState({ duration: this.audio.duration });
     }
 
     togglePlay = () => {
@@ -24,7 +38,9 @@ class AudioPlayer extends React.Component {
     render() {
         return (
             <div>
-                <button onClick={this.togglePlay}>{this.state.play ? 'Pause' : 'Play'}</button>
+                <button onClick={this.togglePlay}>
+                    {this.state.play ? 'Pause' : 'Play'} {this.state.currentTime} / {this.state.duration}
+                </button>
             </div>
         );
     }
