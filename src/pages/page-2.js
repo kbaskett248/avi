f540d97e09f21b2d9b104b4526878af9cc512841
Kenneth Baskett <kbaskett248@gmail.com>
2020-03-07 23:40:23 -0500

    Add image of beluga whale

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = ({ data }) => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Img fluid={data.primaryImage.childImageSharp.fluid} 
+         alt="Beluga whale swimming upside down"/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
+
+export const pageQuery = graphql`
+  query {
+    primaryImage: file(relativePath: { eq: "beluga1.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }`
 
 export default SecondPage
